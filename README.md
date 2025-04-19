fb88db800e1d22b2751e188b109af46087eef763
Fauzia Nyale <fauzianyale001@gmail.com>
2025-04-19 16:03:44 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Props Checkpoint
+
+https://github.com/DBusuru/React_Props_Checkpoint.git
---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.aupfzoZSYevbP0emSKrP7wHaE7%26pid%3DApi&f=1&ipt=66699ecf00c5efc1c475d4484611c1d0cc684130bc7bd6515ffe87178c2013cb&ipo=images",
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIF.U6%252fhnd4SQyq3UZzty4PbLA%26pid%3DApi&f=1&ipt=8d1c1f90e9ec067585874ac21fdd0582ed52ea50aed1fa196171da1cea973335&ipo=images",
+    },
+    {
+      name: "Kevin De Bruyne",
+      team: "Manchester City",
+      nationality: "Belgium",
+      jerseyNumber: 17,
+      age: 32,
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.RFxC58cv0ALOtuk4rGP-MAHaE7%26pid%3DApi&f=1&ipt=9f740fffccf80250c496e758efc918baa071d5f905871345a6057764bee9195a&ipo=images",
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.jYp_OFIrYsauxXRd8KowRgHaE7%26pid%3DApi&f=1&ipt=53218daa32d7525c49d3e7d84f7abe6e51f368edef403b7dd1ee190963e3348b&ipo=images",
+    },
+  ];
+  
+  export default players;
+  
